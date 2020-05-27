e17fc246a004a1b331e85cc777a3b5208bfa81c3
Francis <francispham89@gmail.com>
2020-05-26 22:54:12 -0700

    Implemented createPages Gatsby NodeAPI for Creating Gatsby Pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,31 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require("path")
+
+exports.createPages = async ({ graphql, actions: { createPage }}) => {
+  const pages = await graphql(`
+    {
+      allShopifyProduct {
+        edges {
+          node {
+            id
+            handle
+          }
+        }
+      }
+    }
+  `);
+
+  pages.data.allShopifyProduct.edges.forEach(edge => {
+    createPage({
+      path: `/products/${edge.node.handle}`,
+      component: path.resolve('./src/pages/page-2.js'),
+      context: {
+        id: edge.node.id,
+        handle: edge.node.handle
+      }
+    })
+  })
+}
