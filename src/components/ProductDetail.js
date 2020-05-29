6f9988f5233d5db94424eb2b3bcdd0f954b61aff
Francis <francispham89@gmail.com>
2020-05-29 12:29:58 -0700

    Installed & Setup @shopify/buy-button-js & js-base64 Packages

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,6 +1,13 @@
 import React, {useState} from 'react'
+import ShopifyBuy from '@shopify/buy-button-js'
 
 import Img from 'gatsby-image'
+
+const client = ShopifyBuy.buildClient({
+  domain: 'viet-eat.myshopify.com',
+  storefrontAccessToken: '636e986360bb835928fdedfda57407ad',
+});
+const ui = ShopifyBuy.Ui.init(client);
 
 const ProductDetail = ({ product }) => {
   const [SelectedVariant, setVariant] = useState(product.variants[0]);
@@ -11,18 +18,20 @@ const ProductDetail = ({ product }) => {
       <Img fixed={product.images[0].localFile.childImageSharp.fixed} />
       <p>{product.description}</p>
       <p>${SelectedVariant.price}</p>
-      <select 
+      <select
         onChange={e => {
-          const selected = product.variants.filter(variant => variant.sku === e.target.value);
-          setVariant(selected[0]);
-        }} 
+          const selected = product.variants.filter(
+            variant => variant.sku === e.target.value
+          )
+          setVariant(selected[0])
+        }}
         value={SelectedVariant.sku}
       >
-        {
-          product.variants.map(variant => (
-            <option key={variant.id} value={variant.sku}>{variant.title}</option>
-          ))
-        }
+        {product.variants.map(variant => (
+          <option key={variant.id} value={variant.sku}>
+            {variant.title}
+          </option>
+        ))}
       </select>
     </div>
   )
