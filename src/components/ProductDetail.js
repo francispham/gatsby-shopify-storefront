37c1eaa8eca678e0a335335ced2fe0b4a69a1c5d
Francis <francispham89@gmail.com>
2020-05-29 11:27:11 -0700

    Displaying Single Product Data: Implemented useState to create Dynamic Template for Updating Product's Variants

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 import Img from 'gatsby-image'
 
 const ProductDetail = ({ product }) => {
+  const [SelectedVariant, setVariant] = useState(product.variants[0]);
+
   return (
     <div>
       <h1>{product.title}</h1>
       <Img fixed={product.images[0].localFile.childImageSharp.fixed} />
       <p>{product.description}</p>
-      <select>
+      <p>${SelectedVariant.price}</p>
+      <select 
+        onChange={e => {
+          const selected = product.variants.filter(variant => variant.sku === e.target.value);
+          setVariant(selected[0]);
+        }} 
+        value={SelectedVariant.sku}
+      >
         {
           product.variants.map(variant => (
             <option key={variant.id} value={variant.sku}>{variant.title}</option>
