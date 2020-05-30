434b36ccb20e7a1e1af9bfc93cddac9320969bee
Francis <francispham89@gmail.com>
2020-05-29 17:37:00 -0700

    Shopify Buy Now Button: Implemented Checkout Process for each Product's variant & Acquired webUrl from Shopify Checkout API

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -6,11 +6,19 @@ import { StoreContext } from "../context/StoreContext"
 const ProductDetail = ({ product }) => {
   const [SelectedVariant, setVariant] = useState(product.variants[0]);
   const { client } = useContext(StoreContext);
-  console.log('client:', client);
 
   const addToCart = async () => {
     const newCheckout = await client.checkout.create();
-    console.log('newCheckout:', newCheckout)
+    console.log(product.variants[0].id);  // Not the variant ID
+    // => Shopify__ProductVariant__Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zNDQzNTE5ODAyNTg5Mw==
+    const lineItems = [
+      {
+        variantId: product.variants[0].id.replace("Shopify__ProductVariant__", ""), // Getting real variant ID
+        quantity: 1,
+      },
+    ]
+    const addItems = await client.checkout.addLineItems(newCheckout.id, lineItems);
+    console.log('addItems:', addItems.webUrl);
   };
   addToCart();
 
